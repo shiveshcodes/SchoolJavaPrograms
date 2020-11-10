0e42625b5fde5bcb5bcf31226fd96ae4619a1b79
shiveshcodes <74242886+shiveshcodes@users.noreply.github.com>
2020-11-10 16:37:19 +0530

    Update Count101.java

--- Count101.java
+++ Count101.java
@@ -2,11 +2,11 @@ import java.io.*;
 public class Count101 {
     public static void main(String[] args) {
         try{
-            BufferedReader br=new BufferedReader(new FileReader("sample.txt"));
-            String line; int count=0;
-            while((line=br.readLine())!=null)
-            count+=line.replace("1", "11").split("101").length-1;
-            System.out.println("Number Of Occurences of '101': "+count);
-        }catch(IOException e){}        
+            BufferedReader br=new BufferedReader(new FileReader("sample.txt")); // Attaching a reader to the file
+            String line; int count=0;// stores line and count of pattern occurence
+            while((line=br.readLine())!=null) // Extracts the different lines in a file
+            count+=line.replace("1", "11").split("101").length-1; // 1's are doubled to identify shared set of pattern occurence
+            System.out.println("Number Of Occurences of '101': "+count); // Display the count of pattern occurence
+        }catch(IOException e){System.out.println(e);} // Displays error object's message  
     }
 }
