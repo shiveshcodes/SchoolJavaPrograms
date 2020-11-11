488b4e1c0ffbe5a532e23f641c0ad3e1504492e3
Shivesh Gupta <74242886+shiveshcodes@users.noreply.github.com>
2020-11-11 19:51:54 +0530

    Add files via upload

---
+++ VerticalFloyd.java
@@ -0,0 +1,22 @@
+package JavaPrograms;
+import java.io.*;
+class VerticalFloyd
+{
+    public static void main(String[] args) throws IOException
+    {
+        PrintWriter pw = new PrintWriter(new FileWriter("Pattern.txt"));
+        int n=10;
+        int prev;
+        for(int i=1;i<=n;i++)
+        {
+            prev=i-n;
+            for(int j=1;j<=i;j++)
+            {
+               pw.print((prev=prev+(n-j+1)));
+               if(!(j==i)) pw.print(" ");
+            }
+            pw.println();
+        }
+        pw.close();
+    }
+}
